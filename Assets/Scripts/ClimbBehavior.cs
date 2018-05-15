9aba6d79f8e97a3ba81e306cc2aba50a47acfc00
Taylor Grote <grotese@gmail.com>
2018-05-14 20:40:07 -0500

    Initial Commit

---
+++ Assets/Scripts/ClimbBehavior.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbBehavior : MonoBehaviour 
+{
+	// On Climb Button, see if there is a collision with the colission checker and then climb if able
+}
